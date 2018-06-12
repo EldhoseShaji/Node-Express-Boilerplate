8eb2947d0406c8e777a24f3ef8d1645d4b68494c
Eldhose Shaji <eldhoseshaji1997@gmail.com>
2018-06-12 00:16:14 -0400

    Added debug

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
 
 var app = express();
 
@@ -7,5 +9,5 @@ app.get('/',function(req, res){
 })
 
 app.listen(3000, function(){
-    console.log('Listening on port 3000');
+    console.log(`Listening on port ${chalk.green('3000')}`);
 });
