4531a769061590a961516326caed7c26eed02dbe
Eldhose Shaji <eldhoseshaji1997@gmail.com>
2018-06-19 22:47:59 -0400

    Fixed a small issue with debug

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.get('/',function(req, res){
 })
 
 app.listen(3000, function(){
-    console.log(`Listening on port ${chalk.green('3000')}`);
+    debug(`Listening on port ${chalk.green('3000')}`);
 });
